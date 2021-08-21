34d7176a739a93582a6cf57a211f32b1a3837dac
Kam <kameronkales@gmail.com>
2021-08-21 16:05:03 -0400

    Testing

---
+++ examples/react-example/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { FieldType, DataField } from "@holdequity/hardin";
+
+function App() {
+  console.log({ FieldType });
+  console.log("DataField", DataField);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
